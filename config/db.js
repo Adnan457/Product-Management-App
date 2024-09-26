509100b41733c8493a3bb310a6c007a147d3dd25
Muhammad Adnan Ayub <109515477+Adnan457@users.noreply.github.com>
2024-09-26 14:44:53 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
       `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Errro in  ${error}`.bgRed.white);
   }
 };
 
